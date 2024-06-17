38d6d11e2566d6f0d05bf751a6c3c2e464d41e76
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-06-17 14:48:02 +0530

    basics

---
+++ Basics_Cpp/01 _basics.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int main (){
+    int a ;
+    cin >>a;
+    cout <<a;
+      return 0;
+}
+//////////////////////////////////////////
+#include<bits/stdc++.h> 
+//using namespace std; 
+
+int main (){
+    int a ;
+    cin >>a;
+    cout <<a;
+      return 0;
+}
+//////// give error //////////////////////
+#include<bits/stdc++.h> 
+//using namespace std; 
+
+int main (){
+    int a ;
+    std::cin >>a;
+    std::cout <<a;
+      return 0;
+}
+///////////////////////////////////////////
+//function//
+//void function 
+#include<bits/stdc++.h> 
+using namespace std; 
+
+void print (){ // void fn will not return any thing
+    cout<<"siddharth";
+}
+int main (){
+    print();
+    return 0;
+} 
+//////////////////////////////////////////////
+#include<bits/stdc++.h> 
+using namespace std; 
+
+void print (){
+    cout << "siddharth";
+}
+ int sum (int a, int b){  // written type function
+    return a+b;
+ }
+ int main (){
+    print();
+    int s= sum (1,5);
+    cout << s;
+    return 0;
+ }
+
+
+
+
+
+
+
+
+
+
