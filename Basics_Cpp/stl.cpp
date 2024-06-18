92886c02424457958e3e1323527ea8e76677c217
siddharth goyal <113788213+siddharthgoyal00@users.noreply.github.com>
2024-06-18 21:52:23 +0530

    stl

--- Basics_Cpp/stl.cpp
+++ Basics_Cpp/stl.cpp
@@ -1,1 +1,47 @@
+ #include<bits/stdc++.h> 
+using namespace std; 
  
+
+
+
+
+
+
+
+
+ void explainPair (){
+    pair <int, int> p ={1,2}; // it could be any data type 
+     
+    cout << p.first << " " << p.second; // accesss the pair
+    
+    // nested pair for storing more then two //
+
+    pair<int, pair<int, int>> q = {1,{2,3}};
+
+    cout << q.first << " " << q.second.second <<" " << q.second.first;
+    
+    pair<int, int> arr[] = { {1,2}, {3,4}}; //array using pair as a data type
+    cout << arr[1].second;
+    // out put will be 4
+
+ }
+
+//////////////////////////////////////////////////////
+
+ void explainVector(){
+      vector<int> v; // creates empty container 
+      v.push_back(1);
+      v.emplace_back(2);
+
+      vector<pair<int,int>>vec;
+
+       v.push_back({1,2});
+       v.emplace_back(1,2);
+
+       vector<int>v(5,100); //{100,100,100,100,100} five instances of 100
+
+       vector<int> v(5); //{" " ," " ," " ," " ," " }
+         
+       vector<int> v1(5,20); //{20,20,20,20,20}
+       vector <int> v2(v1);
+        }
